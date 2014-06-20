a7239881835439cddbc8409e2277b4d82be3288c
csanchez <csanchez@carnival.com>
2014-06-20 13:01:03 -0400

    rename test class

--- NullableOperations/NullableOperations/NullableArithmeticOperations.cs
+++ NullableOperations/NullableOperations/NullableArithmeticOperations.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace NullableOperations
 {
     [TestClass]
-    public class NullableOperations
+    public class NullableArithmeticOperations
     {
         [TestMethod]
         public void NullablesWithValues()
