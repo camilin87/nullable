5eeafa22a92358775be1e4859f18664e80647db1
csanchez <csanchez@carnival.com>
2014-06-20 13:07:01 -0400

    NullablesWithValues

--- NullableOperations/NullableOperations/NullableUnaryOperations.cs
+++ NullableOperations/NullableOperations/NullableUnaryOperations.cs
@@ -5,6 +5,15 @@ namespace NullableOperations
     [TestClass]
     public class NullableUnaryOperations
     {
+        [TestMethod]
+        public void NullablesWithValues()
+        {
+            decimal? money1 = 5;
+            decimal? money2 = 5;
+
+            Assert.AreEqual(6, ++money1);
+            Assert.AreEqual(4, --money2);
+        }
         
     }
 }
