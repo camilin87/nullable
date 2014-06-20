3de8bdb5fd7984b9537df9822e58f924c0218402
csanchez <csanchez@carnival.com>
2014-06-20 12:56:41 -0400

    NullablesWithValues

--- NullableOperations/NullableOperations/NullableOperations.cs
+++ NullableOperations/NullableOperations/NullableOperations.cs
@@ -6,6 +6,16 @@ namespace NullableOperations
     [TestClass]
     public class NullableOperations
     {
-        
+        [TestMethod]
+        public void NullablesWithValues()
+        {
+            decimal? money1 = 5;
+            decimal? money2 = 6;
+
+            Assert.AreEqual(11, money1 + money2);
+            Assert.AreEqual(-1, money1 - money2);
+            Assert.AreEqual(30, money1 * money2);
+            Assert.IsTrue(decimal.Equals(0.8333333333333333333333333333M, (money1 / money2).Value));
+        }
     }
 }
